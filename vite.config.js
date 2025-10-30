0da169fc2db89996b80f7017d0466a8d1090a2cd
SinethNR <nimhanrds.22@uom.lk>
2025-10-30 12:23:55 +0530

    feat: Update dependencies and enhance Vite configuration for Emotion and Babel integration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,28 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      '@emotion/react',
+      '@emotion/styled',
+      '@emotion/cache',
+      '@emotion/sheet',
+      '@emotion/serialize',
+      '@babel/runtime/helpers/typeof',
+      '@babel/runtime/helpers/slicedToArray',
+      '@babel/runtime/helpers/asyncToGenerator',
+      '@babel/runtime/helpers/defineProperty',
+      '@babel/runtime/helpers/esm/extends',
+      '@babel/runtime/helpers/esm/objectWithoutPropertiesLoose',
+      '@babel/runtime/helpers/esm/inheritsLoose',
+      '@babel/runtime/helpers/esm/assertThisInitialized'
+    ]
+  },
+  resolve: {
+    alias: {
+      '@emotion/cache': '@emotion/cache',
+      '@emotion/sheet': '@emotion/sheet',
+      '@emotion/serialize': '@emotion/serialize'
+    }
+  }
 })
