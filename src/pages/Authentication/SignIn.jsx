28897dbc4df8d91a7b14c881d5bbdc328f278f76
minulck <minulck@gmail.com>
2025-10-31 20:21:44 +0530

    Reapply "Merge pull request #25 from AxleWorks/authentication"
    This reverts commit 35cbf95d5c2b1a80285df9ef594f24a7375c690b.

---
+++ src/pages/Authentication/SignIn.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect } from "react";
+import LoginComponent from "../../components/auth/SignIn";
+
+const Login = () => {
+  return <LoginComponent />;
+};
+
+export default Login;
